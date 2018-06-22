0345b8d0bf427a24608afd1ec43257634604db0d
houlder.hambalaye <hnahoulder@gmail.com>
2018-06-22 15:33:03 +0200

    router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
 
 
 import {AppComponent} from './app.component';
@@ -9,8 +10,19 @@ import {SearchComponent} from './search/search.component';
 import {JobListComponent} from './job-list/job-list.component';
 import {JobService} from './services/job.service';
 import {JobAddFormComponent} from './job-add-form/job-add-form.component';
-import { DaysAgoPipe } from './pipes/days-ago.pipe';
+import {DaysAgoPipe} from './pipes/days-ago.pipe';
+import {HomeComponent} from './home/home.component';
+import {JobDetailsComponent} from './job-details/job-details.component';
+import { AboutComponent } from './about/about.component';
 
+
+const routes = [
+    {path: '', component: HomeComponent},
+    {path: 'jobs/add', component: JobAddFormComponent},
+    {path: 'jobs/:id', component: JobDetailsComponent},
+    {path: 'jobs', component: JobListComponent},
+    {path: 'about', component: AboutComponent}
+];
 
 @NgModule({
     declarations: [
@@ -18,13 +30,17 @@ import { DaysAgoPipe } from './pipes/days-ago.pipe';
         SearchComponent,
         JobListComponent,
         JobAddFormComponent,
-        DaysAgoPipe
+        DaysAgoPipe,
+        HomeComponent,
+        JobDetailsComponent,
+        AboutComponent
     ],
     imports: [
         BrowserModule,
         HttpClientModule,
         FormsModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        RouterModule.forRoot(routes)
     ],
     providers: [JobService],
     bootstrap: [AppComponent]
