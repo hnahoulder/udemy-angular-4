9e1dbd6aa21f3a9e44894cce71fd2ff50e7490ca
houlder.hambalaye <hnahoulder@gmail.com>
2018-06-22 15:33:03 +0200

    router

--- src/app/services/job.service.ts
+++ src/app/services/job.service.ts
@@ -1,9 +1,12 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Subject} from 'rxjs/Rx';
+import 'rxjs/add/operator/do';
+import {Subject} from 'Rxjs/Rx';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class JobService {
+    initialJobs: any = [];
     jobs: any = [];
     jobsSubject = new Subject();
 
@@ -11,13 +14,25 @@ export class JobService {
     }
 
     getJobs() {
-        return this.http.get('data/jobs.json');
+        if (this.jobs.length > 0 && this.initialJobs.length > 0) {
+            console.log('case if');
+            return Observable.of([...this.jobs, ...this.initialJobs]);
+        } else if (this.jobs.length > 0 && this.initialJobs.length === 0) {
+            console.log('case else if');
+            return this.http.get('data/jobs.json').do(data => {
+                this.initialJobs = data;
+                this.jobs = [...this.jobs, ...this.initialJobs];
+            });
+        } else {
+            console.log('else');
+            return this.http.get('data/jobs.json').do(data => this.initialJobs = data);
+        }
     }
 
     addJob(jobData) {
         jobData.id = Date.now();
+        this.jobs = [jobData, ...this.jobs];
         return this.jobsSubject.next(jobData);
-
     }
 
 }
