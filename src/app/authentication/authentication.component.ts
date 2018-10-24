3ee1111d820aac57413771974d3fddd425ae6c32
hnah <hnahoulder@gmail.com>
2018-10-24 22:56:04 +0200

    section 6 partie 40

--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -7,22 +7,37 @@ import {AuthService} from '../services/auth.service';
     styleUrls: ['./authentication.component.css']
 })
 export class AuthenticationComponent implements OnInit {
+    jbbData = null;
+    isAuthenticated = false;
+    welcomeMessage = '';
 
     constructor(private authService: AuthService) {
     }
 
     ngOnInit() {
+        if (localStorage.getItem('jbb-data')) {
+            this.refreshFlags();
+        }
+    }
+
+    refreshFlags() {
+
+        this.isAuthenticated = true;
+        this.welcomeMessage = 'Bienvenue';
     }
 
     login(formData) {
         console.log(formData);
         this.authService.login(formData)
-            .subscribe(data => this.handleLoginSuccess,
-                error => this.handleLoginFailure);
+            .subscribe(data => this.handleLoginSuccess(data),
+                error => this.handleLoginFailure(error));
     }
 
     handleLoginSuccess(data) {
         console.log('Success ', data);
+        this.jbbData = data;
+        this.refreshFlags();
+        localStorage.setItem('jbb-data', JSON.stringify(data));
     }
 
     handleLoginFailure(error) {
